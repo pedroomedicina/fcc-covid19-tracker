b4ab35bc0f40cc171e851ddb335369c9eff178b1
Pedro Medina <pmedina@kuantum.cl>
2020-06-02 21:26:48 -0400

    about titles page titles

--- src/routes/[state].svelte
+++ src/routes/[state].svelte
@@ -10,9 +10,15 @@
     export let state;
 </script>
 
-<h1>
-    {state}
-</h1>
+<svelte:head>
+    <title>Covid 19 Tracker - {state} </title>
+</svelte:head>
+
+<div class="section header">
+    <div class="container">
+        <h1>Covid 19 - {state}</h1>
+    </div>
+</div>
 
 <CovidStat />
 <CovidChart />
---
+++ src/routes/about.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+    <title>About Covid 19 Tracker on the US</title>
+</svelte:head>
+
+<div class="section header">
+    <div class="container">
+        <h1>About</h1>
+        <p>
+            The data for this site came from <a href="https://covidtracking.com">Covid Tracking Project</a>. You can learn more about it visiting their site. 
+        </p>
+    </div>
+</div>
